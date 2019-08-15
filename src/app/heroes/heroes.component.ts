f145d4d480438ba8ce6e4baf651058fa8116b19a
Andrew Petti <andrewpetti@Honor-macMini.chemies.local>
2019-08-14 21:39:39 -0600

    List of heroes and activate details when selecting

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+import { HEROES } from "src/mock-heroes";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
 
-  hero: Hero = { 
-    id:1,
-    name:'Windstorm'
-  };
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
 }
